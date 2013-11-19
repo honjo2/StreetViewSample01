0594c25cacfb6c8dc73828e036b9ee85f403e7ef
honjo2 <honjo2@gmail.com>
2013-11-19 14:01:36 +0900

    Initial Commit

---
+++ StreetViewSample01/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StreetViewSample01
+//
+//  Created by honjyo on 2013/11/19.
+//  Copyright (c) 2013年 honjyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
